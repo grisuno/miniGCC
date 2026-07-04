7f33b4a38d8592295b3f61862311ec94e28d6f67
Grisuno <grisuno@gmail.com>
2026-07-04 02:20:20 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
 gcc -std=c99 -Wall -Wextra -O2 -o minigcc minigcc.c                                                                                                                                        ─╯
-./minigcc test.c > test.s
-as test.s -o test.o
-ld test.o -o test
-./test
+./minigcc minigcc.c > minigcc.s
+as minigcc.s -o minigcc.o
+gcc -no-pie -nostdlib minigcc.o -o minigccg2 -lc
+./minigccg2 test.c                                                                                                                                                                           
 echo $? 
